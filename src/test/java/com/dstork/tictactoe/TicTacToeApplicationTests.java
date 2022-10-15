b73e83722faf2cee4e3c9eb30d79e36ba1c3e122
2022-DEV1-067 <major.dodge99@gmail.com>
2022-10-15 15:06:09 +0100

    initial commit

---
+++ src/test/java/com/dstork/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dstork.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
