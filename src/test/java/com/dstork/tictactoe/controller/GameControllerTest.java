11e62a564d0326440b5bd42eb1fbcb7566ce588a
2022-DEV1-067 <major.dodge99@gmail.com>
2022-10-16 19:07:43 +0100

    Adding Test signature methods for GameController

--- src/test/java/com/dstork/tictactoe/controller/GameControllerTest.java
+++ src/test/java/com/dstork/tictactoe/controller/GameControllerTest.java
@@ -1,10 +1,36 @@
 package com.dstork.tictactoe.controller;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.dstork.tictactoe.services.GameService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GameControllerTest {
 
+    @InjectMocks
+    GameController gameController;
 
+    @Mock
+    GameService gameService;
+
+    @Test
+    void getGameById() {
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void cancelGame() {
+    }
+
+    @Test
+    void play() {
+    }
 }
