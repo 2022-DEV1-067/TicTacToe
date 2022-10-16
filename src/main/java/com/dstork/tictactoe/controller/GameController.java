9b5bea2377354fe8392453caa2fdc2682796be69
2022-DEV1-067 <major.dodge99@gmail.com>
2022-10-16 21:58:17 +0100

    Implementation gameController : getGameById

--- src/main/java/com/dstork/tictactoe/controller/GameController.java
+++ src/main/java/com/dstork/tictactoe/controller/GameController.java
@@ -3,9 +3,13 @@ package com.dstork.tictactoe.controller;
 import com.dstork.tictactoe.dto.GameDTO;
 import com.dstork.tictactoe.dto.GameMoveDTO;
 import com.dstork.tictactoe.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +22,25 @@ import java.net.URI;
 public class GameController {
 
     private final GameService gameService;
-    Logger logger = LogManager.getLogger(GameController.class);
-    private final String GAME = "/TicTacToe/Game/";
+    private static final String GAME = "/TicTacToe/Game/";
 
+
+    @Operation(description = "Retrieving a game")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Game Retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "The game is not found")})
     @GetMapping(path = "/{Id}", produces = "application/json")
     public ResponseEntity<GameDTO> getGameById(@PathVariable Long Id) {
-        return null;
+        return gameService.findGameById(Id).map(
+                gameDTO -> ResponseEntity
+                        .ok()
+                        .header(GAME + Id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .eTag(Long.toString(gameDTO.getVersion()))
+                        .body(gameDTO)).orElse(ResponseEntity.notFound().build());
     }
+
+
+
     @PostMapping(path = "/startGame", produces = "application/json", consumes = "application/json")
     public ResponseEntity<GameDTO> startGame(@RequestBody @Valid String playerLogin) {
         return null;
