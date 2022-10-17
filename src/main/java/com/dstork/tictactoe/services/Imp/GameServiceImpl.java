2aff5fd48b4b342d532483d3732fa0166c7db462
2022-DEV1-067 <major.dodge99@gmail.com>
2022-10-17 19:11:28 +0100

    Implementation of GameService : makeGameMove

--- src/main/java/com/dstork/tictactoe/services/Imp/GameServiceImpl.java
+++ src/main/java/com/dstork/tictactoe/services/Imp/GameServiceImpl.java
@@ -3,6 +3,7 @@ package com.dstork.tictactoe.services.Imp;
 import com.dstork.tictactoe.dto.GameDTO;
 import com.dstork.tictactoe.dto.GameMoveDTO;
 import com.dstork.tictactoe.enums.GameStatus;
+import com.dstork.tictactoe.exceptions.BadRequestException;
 import com.dstork.tictactoe.exceptions.NotAllowedException;
 import com.dstork.tictactoe.exceptions.ResourceNotFoundException;
 import com.dstork.tictactoe.mapper.GameMapper;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.dstork.tictactoe.enums.GameStatus.*;
+import static com.dstork.tictactoe.enums.PositionValue.*;
 
 @Service
 @RequiredArgsConstructor
@@ -37,7 +39,7 @@ public class GameServiceImpl implements GameService {
     @Override
     public Optional<GameDTO> findGameById(Long id) {
         Game game = gameRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Game with Id: " + id + " Not Found"));
+                .orElseThrow(() -> new ResourceNotFoundException(" Game with Id: " + id + " Not Found."));
 
         logger.info("Game Details with Id : {}  Retrieved.", game.getId());
         return Optional.of(gameMapper.mapGameToGameDTO(game));
@@ -78,12 +80,99 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public Optional<GameDTO> makeGameMove(GameMoveDTO gameMoveDTO) {
-        return null;
+
+        Game game = gameRepository.findById(gameMoveDTO.getGameId()).orElseThrow(
+                () -> new ResourceNotFoundException("Game with Id: " + gameMoveDTO.getGameId() + " Not Found"));
+
+        checkGameMove(gameMoveDTO, game);
+
+        if (game.getPlayerX().getLogin().equals(gameMoveDTO.getPlayerLogin())) {
+            game.getGameBoard().put(gameMoveDTO.getPosition(), X);
+            game.setGameStatus(O_TURN);
+            logger.info("Player X Plays the position:  {}.", gameMoveDTO.getPosition());
+
+        } else {
+            game.getGameBoard().put(gameMoveDTO.getPosition(), O);
+            game.setGameStatus(X_TURN);
+            logger.info("Player O Plays the position {}.", gameMoveDTO.getPosition());
+
+        }
+        GameStatus gameStatus = GameUtil.checkBoardState(game.getGameBoard());
+        if (gameStatus != null) {
+            switch (gameStatus) {
+                case X_WINS:
+                    game.setGameStatus(X_WINS);
+                    logger.info("Game ended the winner is  {}.", game.getPlayerX().getLogin());
+                    break;
+                case O_WINS:
+                    game.setGameStatus(O_WINS);
+                    logger.info("Game ended the winner is  {}.", game.getPlayerO().getLogin());
+                    break;
+                case DRAW:
+                    game.setGameStatus(DRAW);
+                    logger.info("Game ended , It is a draw");
+                    break;
+                default:
+            }
+        }
+
+        game = gameRepository.save(game);
+        GameUtil.printGameBoard(game.getGameBoard());
+
+        return Optional.of(gameMapper.mapGameToGameDTO(game));
     }
+
+    public void checkGameMove(GameMoveDTO gameMoveDTO, Game game) {
+
+        playerRepository.findByLogin(gameMoveDTO.getPlayerLogin())
+                .orElseThrow(() -> new ResourceNotFoundException("Player with login " + gameMoveDTO.getPlayerLogin() + " Not found"));
+
+        if (!isPlayerInGame(gameMoveDTO, game)) {
+            throw new NotAllowedException("Player with login:  " + game.getPlayerX().getLogin() + " Is not in the game");
+        }
+        if (isGameDone(game)) {
+            throw new NotAllowedException("Game with Id : " + game.getId() + " Already ended");
+        }
+
+        if (!isPlayerSTurn(game, gameMoveDTO)) {
+            throw new BadRequestException("It is not the player:  " + game.getPlayerX().getLogin() + " 'S turn");
+        }
+        if (game.getGameStatus() == PENDING) {
+            throw new NotAllowedException("Game with Id: " + game.getId() + "Is still waiting for the second player");
+        }
+        if (game.getGameStatus() == CANCELLED) {
+            throw new NotAllowedException("Game with Id : " + game.getId() + " Is already cancellled");
+        }
+
+        if (game.getGameBoard().containsValue(gameMoveDTO.getPosition())) {
+            throw new BadRequestException("The position " + gameMoveDTO.getPosition() + " Already filled in");
+        }
+
+    }
+
+    private static boolean isPlayerInGame(GameMoveDTO gameMoveDTO, Game game) {
+        return game.getPlayerX().getLogin().equals(gameMoveDTO.getPlayerLogin()) ||
+                game.getPlayerO().getLogin().equals(gameMoveDTO.getPlayerLogin());
+    }
+
+    private boolean isPlayerSTurn(Game game, GameMoveDTO gameMoveDTO) {
+        return (game.getPlayerX().getLogin().equals(gameMoveDTO.getPlayerLogin()) &&
+                game.getGameStatus().equals(X_TURN) ||
+                game.getPlayerO().getLogin().equals(gameMoveDTO.getPlayerLogin()) &&
+                        game.getGameStatus().equals(O_TURN));
+    }
+
+    private static boolean isGameDone(Game game) {
+        return game.getGameStatus() == X_WINS ||
+                game.getGameStatus() == O_WINS ||
+                game.getGameStatus() == DRAW;
+    }
+
 
     private Game findGameByStatusPENDING() {
         return gameRepository.findByGameStatus(PENDING).stream().findAny().orElse(null);
     }
+
     private static boolean isEmptyList(List<Game> playerGames) {
         return playerGames == null || playerGames.isEmpty();
     }
