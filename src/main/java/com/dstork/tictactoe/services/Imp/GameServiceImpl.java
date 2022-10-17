e84b661e5bfabceb2739e9af65b672818f680c90
2022-DEV1-067 <major.dodge99@gmail.com>
2022-10-17 13:40:30 +0100

    Implementation of GameService : findGameById

--- src/main/java/com/dstork/tictactoe/services/Imp/GameServiceImpl.java
+++ src/main/java/com/dstork/tictactoe/services/Imp/GameServiceImpl.java
@@ -2,10 +2,15 @@ package com.dstork.tictactoe.services.Imp;
 
 import com.dstork.tictactoe.dto.GameDTO;
 import com.dstork.tictactoe.dto.GameMoveDTO;
+import com.dstork.tictactoe.exceptions.ResourceNotFoundException;
+import com.dstork.tictactoe.mapper.GameMapper;
+import com.dstork.tictactoe.model.Game;
 import com.dstork.tictactoe.repository.GameRepository;
 import com.dstork.tictactoe.repository.PlayerRepository;
 import com.dstork.tictactoe.services.GameService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,9 +22,17 @@ public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
     private final PlayerRepository playerRepository;
 
+    Logger logger = LogManager.getLogger(GameServiceImpl.class);
+
+    final GameMapper gameMapper;
+
     @Override
     public Optional<GameDTO> findGameById(Long id) {
-        return null;
+        Game game = gameRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Game with Id: " + id + " Not Found"));
+
+        logger.info("Game Details with Id : {}  Retrieved.", game.getId());
+        return Optional.of(gameMapper.mapGameToGameDTO(game));
     }
 
     @Override
